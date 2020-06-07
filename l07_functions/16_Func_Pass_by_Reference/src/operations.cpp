018e7402e707db093f00aeb1e080bc8fd454cb71
mswilliam <m_s_william@yahoo.fr>
2020-06-07 12:21:31 +0200

    add feature Func_Pass_by_Reference

---
+++ l07_functions/16_Func_Pass_by_Reference/src/operations.cpp
@@ -0,0 +1,15 @@
+/*!
+ *  \brief    Goal: Practice putting functions in a header file.
+ *
+ *  \details  Move the function declaration and definition to the header fi    le. Don't forget to include the header file (main.hpp) in the main program.
+ *
+ *  \author   MANGOUA SOFACK William
+ *  \version  1.0
+ *  \date     2020-06-05
+ *  \copyright GNU Public License.
+ *
+ *
+ *
+ **/
+
+
