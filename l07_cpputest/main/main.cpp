38a62d612c46e77f3932ef25243020023cb98eb9
mswilliam <m_s_william@yahoo.fr>
2020-06-11 01:52:19 +0200

    prepare for testing

---
+++ l07_cpputest/main/main.cpp
@@ -0,0 +1,29 @@
+/*!
+ *  \file main.cpp
+ *  \brief    Goal: Test the operations implementation
+ *
+ *  \details  call operations functions
+ *
+ *  \author   MANGOUA SOFACK William
+ *  \version  1.0
+ *  \date     2020-06-07
+ *  \copyright GNU Public License.
+ *
+ *
+ *
+ **/
+
+#include "../components/operations/include/operations.hpp"
+
+int main() {
+  uint8_t operation = '*';
+  float input1 = 9.8;
+  float input2 = 2.3;
+  float result;
+
+  if (ErrorFunctionHandler::kOk ==  \
+      Calculate(input1, input2, operation, result)) {
+    PrintEquation(input1, input2, operation, result);
+  }
+  return 0;
+}
