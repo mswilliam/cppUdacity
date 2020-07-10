ac7e32bda9b3246da578d36261c2eabd8deb3ed1
mswilliam <m_s_william@yahoo.fr>
2020-07-10 17:10:37 +0200

    udate tests

--- l08_classes/19_Helper_Function_Program/components/gameboard/tests/test.cpp
+++ l08_classes/19_Helper_Function_Program/components/gameboard/tests/test.cpp
@@ -16,58 +16,82 @@
 #include "CppUTest/TestHarness.h"
 #include "../include/gameboard.h"
 
-TEST_GROUP (Addition) {
+TEST_GROUP (SetGameSpaceGroup) {
 
   //  Define data accessible to the test group members here.
 
+  // This global variable is used for all the tests in this group
+  gameboard::Gameboard gl_gameboard;
+
   void setup () {
     //  initialization steps are executed before each TEST
+    for (uint8_t loc_row_index = 0; loc_row_index < gameboard::kGameSpaceSize; ++loc_row_index) {
+      for (uin8_t loc_col_index = 0; loc_col_index < kGameSpaceSize; ++loc_col_index) {
+        gl_gameboard[loc_row_index][loc_col_index] = '-';
+      }
+    }
   }
 
   void tearDown () {
     //  ceanup steps are excuted after each TEST
-  
-};
 
-TEST (Addition, ZeroPlusOne) {
-  uint8_t operation = '+';
-  float input1 = 0.0;
-  float input2 = 1.1;
-  float result;
+  };
 
-  CHECK (operations::ErrorFunctionHandler::kOk == \
-      operations::Calculate(input1, input2, operation, result));
+  TEST (SetGameSpaceGroup, ZeroPlusOne) {
+    uint8_t operation = '+';
+    float input1 = 0.0;
+    float input2 = 1.1;
+    float result;
 
-  LONGS_EQUAL (1.0, result);
-}
+    CHECK (operations::ErrorFunctionHandler::kOk == \
+        operations::Calculate(input1, input2, operation, result));
 
-TEST (Addition, ZeroPlusZero) {
-  uint8_t operation = '+';
-  float input1 = 0.0;
-  float input2 = 0.0;
-  float result;
-
-  CHECK (operations::ErrorFunctionHandler::kOk == \
-      operations::Calculate(input1, input2, operation, result));
-
-  LONGS_EQUAL (0.0, result);
-}
-
-
-
-/*
-
-
-int main() {
-  uint8_t operation = '*';
-  float input1 = 9.8;
-  float input2 = 2.3;
-  float result;
-
-  if (operations::ErrorFunctionHandler::kOk ==  \
-      operations::Calculate(input1, input2, operation, result)) {
-    operations::PrintEquation(input1, input2, operation, result);
+    LONGS_EQUAL (1.0, result);
   }
-  return 0;
-}
-*/
+
+  TEST (SetGameSpaceGroup, setOneCharacter) {
+    uint8_t loc_row_upate_index {0};
+    uint8_t loc_col_update_index {0};
+    int8_t loc_updated_value {'a'};
+
+    gameboard::Gameboard loc_gameboard;
+
+    // copy the gameboard befor modification
+    for (uint8_t loc_row_index = 0; \
+        loc_row_index < gameboard::kGameSpaceSize; \
+        ++loc_row_index) {
+      for (uin8_t loc_col_index = 0; \
+          loc_col_index < kGameSpaceSize; \
+          ++loc_col_index) {
+        loc_gameboard.setGameSpace(loc_row_index, \
+            loc_col_index, \
+            gl_gameboard.getGameSpace(loc_row_index, loc_col_index));
+      }
+    }
+
+    // set the value
+    loc_gameboard.setGameSpace(loc_row_upate_index, \
+        loc_col_update_index, \
+        loc_updated_value);
+
+    // test
+    for (uint8_t loc_row_index = 0; \
+        loc_row_index < gameboard::kGameSpace Size; \
+        ++loc_row_index) {
+      for (uin8_t loc_col_index = 0; \
+          loc_col_index < kGameSpaceSize; \
+          ++loc_col_index) {
+        loc_gameboard.setGameSpace(loc_row_index, \
+            loc_col_index, \
+            gl_gameboard.getGameSpace(loc_row_index, loc_col_index);
+            }
+            }
+
+            CHECK (operations::ErrorFunctionHandler::kOk == \
+              operations::Calculate(input1, input2, operation, result));
+
+            LONGS_EQUAL (0.0, result);
+            }
+
+
+
